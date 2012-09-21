57a2ba6c155a0555797da5567bcdcc810af72642
Paulo Geyer <paulogeyer@gmail.com>
2012-09-21 12:49:55 -0300

    using system email

--- app/mailers/credits_mailer.rb
+++ app/mailers/credits_mailer.rb
@@ -1,10 +1,10 @@
 class CreditsMailer < ActionMailer::Base
   include ERB::Util
-  default :from => "Catarse <system@catarse.me>"
+  default :from => "#{t('site.name')} <#{t('site.email.system')}>"
 
   def request_refund_from(backer)
     @backer = backer
     @user = backer.user
-    mail(:to => 'financeiro@catarse.me', :subject => I18n.t('credits_mailer.request_refund_from.subject', :name => @user.name))
+    mail(:to => t('site.email.payments'), :subject => I18n.t('credits_mailer.request_refund_from.subject', :name => @user.name))
   end
 end
