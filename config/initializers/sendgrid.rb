093a94b9f2c7f57f5d40edfccc480ab1976d726c
Paulo Geyer <paulogeyer@gmail.com>
2012-09-21 12:09:40 -0300

    sendgrid configuration

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -4,8 +4,9 @@ begin
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'jlvt',
-    :password       =>  Configuration[:sendgrid],
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :enable_starttls_auto => true,
     :domain         => 'heroku.com'
     }
     ActionMailer::Base.delivery_method = :smtp
