153269a14102981973dd2127c5bd8cf259fa9212
Paulo Geyer <paulogeyer@gmail.com>
2012-12-05 04:22:49 -0200

    should show only recommended and not expired projects in the header

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -32,7 +32,8 @@ class ProjectsController < ApplicationController
         @expiring             = presenter.expiring
         @recent               = presenter.recent
 
-        @fourth_project = Project.home_page.visible.last unless @recommended_project
+        @fourth_project = Project.home_page.not_expired.recommended.visible.last unless @recommended_project
+        @fourth_project = Project.home_page.recommended.visible.last unless @recommended_project
 
         # @blog_posts = Blog.fetch_last_posts.inject([]) do |total,item| 
         #   if total.size < 2
