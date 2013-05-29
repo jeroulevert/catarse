c8af913df25aae81f1a32e2b60174271e2f3bf28
Paulo Geyer <paulogeyer@gmail.com>
2013-05-29 13:56:11 -0300

    changing user city from text to string in the admin options

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -28,7 +28,7 @@ ActiveAdmin.register User do
       f.input :newsletter
       f.input :project_updates
       f.input :admin
-      f.input :address_city
+      f.input :address_city, :as => :string
       f.input :locale, :as => :string
     end
 
